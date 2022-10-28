97a8ddbb7ec6ec2693e0346aa4dd10f210bc0a80
NicoleGuimaraes <57530790+NicoleGuimaraes@users.noreply.github.com>
2022-10-27 23:59:10 -0300

    Update redisServer.py
    Pequena alteração no código anterior

--- exemple-reddis/redisServer.py
+++ exemple-reddis/redisServer.py
@@ -1,29 +1,21 @@
 import redis
 
-port = 4000
-
-serv = redis.Redis(host='localhost', port=port)
+port = 6379
+serv = redis.Redis(host='localhost', port=port, charset ="utf-8", decode_responses=True)
 pool = redis.ConnectionPool(host='localhost', port=port, db=0)
 client = redis.Redis(connection_pool=pool)
 
 print('running on port ', port)
-#  def getMsg(self, remetente):
-#         msg = client.get(input()) 
-#         if msg == "exit":
-#           finished = True
-#         else:
-#            print(f'/n voce possui a seguinte menssgem: {msg}/n')
-#         client.set(input('Message: ')) 
 
 
 finished = False
 while not finished:
-        msg = client.get("mg2") 
-        if msg == "exit":
-            finished = True
-        else:
-            print('/n voce possui a seguinte menssgem: {msg}')
-        client.set("mg", input('Message: ') ) 
+  msg = client.get("mg2").key.decode()
+  if msg == "exit":
+    finished = True
+  else:
+    print('\nvoce possui a seguinte menssgem: ', {msg})
+  client.set("mg", input('\n Digite sua Mensagem: ')).key.encode()
 
 client.close()
 serv.close()
